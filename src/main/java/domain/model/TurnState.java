00930bd8851d04967d61d86c722ec2374a236a01
carolineguerraa <carolinejjguerra@gmail.com>
2026-05-13 19:39:42 -0500

    fix: change from tabs to spaces

--- src/main/java/domain/model/TurnState.java
+++ src/main/java/domain/model/TurnState.java
@@ -4,68 +4,68 @@ import java.util.Optional;
 
 public class TurnState {
 
-    private static final int DEFAULT_TURNS = 1;
+	private static final int DEFAULT_TURNS = 1;
 
-    private int turnsToTake;
-    private boolean skipDraw;
-    private boolean isAttacking;
-    private Card pendingAction;
-    private int nopeCount;
+	private int turnsToTake;
+	private boolean skipDraw;
+	private boolean isAttacking;
+	private Card pendingAction;
+	private int nopeCount;
 
-    public TurnState() {
-        reset();
-    }
+	public TurnState() {
+		reset();
+	}
 
-    public void enableSkipDraw() {
-        skipDraw = true;
-    }
+	public void enableSkipDraw() {
+		skipDraw = true;
+	}
 
-    public void startAttack(int turns) {
-        isAttacking = true;
-        turnsToTake = turns;
-    }
+	public void startAttack(int turns) {
+		isAttacking = true;
+		turnsToTake = turns;
+	}
 
-    public void setPendingAction(Card card) {
-        pendingAction = card;
-    }
+	public void setPendingAction(Card card) {
+		pendingAction = card;
+	}
 
-    public void clearPendingAction() {
-        pendingAction = null;
-    }
+	public void clearPendingAction() {
+		pendingAction = null;
+	}
 
-    public void incrementNopeCount() {
-        nopeCount++;
-    }
+	public void incrementNopeCount() {
+		nopeCount++;
+	}
 
-    public void decrementTurns() {
-        turnsToTake = Math.max(0, turnsToTake - 1);
-    }
+	public void decrementTurns() {
+		turnsToTake = Math.max(0, turnsToTake - 1);
+	}
 
-    public boolean shouldSkipDraw() {
-        return skipDraw;
-    }
+	public boolean shouldSkipDraw() {
+		return skipDraw;
+	}
 
-    public boolean isAttacking() {
-        return isAttacking;
-    }
+	public boolean isAttacking() {
+		return isAttacking;
+	}
 
-    public int nopeCount() {
-        return nopeCount;
-    }
+	public int nopeCount() {
+		return nopeCount;
+	}
 
-    public int turnsRemaining() {
-        return turnsToTake;
-    }
+	public int turnsRemaining() {
+		return turnsToTake;
+	}
 
-    public Optional<Card> pendingAction() {
-        return Optional.ofNullable(pendingAction);
-    }
+	public Optional<Card> pendingAction() {
+		return Optional.ofNullable(pendingAction);
+	}
 
-    public void reset() {
-        turnsToTake = DEFAULT_TURNS;
-        skipDraw = false;
-        isAttacking = false;
-        pendingAction = null;
-        nopeCount = 0;
-    }
+	public void reset() {
+		turnsToTake = DEFAULT_TURNS;
+		skipDraw = false;
+		isAttacking = false;
+		pendingAction = null;
+		nopeCount = 0;
+	}
 }
