a9a74af917adebf255477aef9eb0e7e349d4f52f
carolineguerraa <carolinejjguerra@gmail.com>
2026-05-24 15:44:39 -0500

    testing & bva: startGame_ValidMaxPlayers_InitializesWithoutError passes

--- src/test/java/ui/GameControllerTest.java
+++ src/test/java/ui/GameControllerTest.java
@@ -6,12 +6,29 @@ import org.junit.jupiter.api.Test;
 
 public class GameControllerTest {
 
+	private static final int FIVE_PLAYERS_IN_GAME = 5;
+
 	@Test
 	void startGame_ValidMinPlayers_InitializesWithoutError() {
 		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
 		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
 
 		EasyMock.expect(input.promptNumPlayers()).andReturn(2);
+
+		EasyMock.replay(display, input);
+
+		GameController controller = new GameController(display, input);
+		controller.startGame();
+
+		EasyMock.verify(display, input);
+	}
+
+	@Test
+	void startGame_ValidMaxPlayers_InitializesWithoutError() {
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+
+		EasyMock.expect(input.promptNumPlayers()).andReturn(FIVE_PLAYERS_IN_GAME);
 
 		EasyMock.replay(display, input);
 
