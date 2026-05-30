04e340490807f43f6b81b640f5ccc99c6a6a9a55
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-05-30 10:14:51 -0500

    added a main method

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,6 +1,19 @@
 package ui;
 
+import domain.factory.ComboValidator;
+import domain.factory.PlayerInteractionHelper;
+
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
+		GameView view = new GameView();
+		PlayerInteractionHelper helper = new PlayerInteractionHelper(view, new Random());
+		ComboValidator comboValidator = new ComboValidator(helper);
+		GameController controller = new GameController(view, view, comboValidator);
+		controller.startGame();
+		while (controller.isGameActive()) {
+			controller.playATurn();
+		}
 	}
 }
