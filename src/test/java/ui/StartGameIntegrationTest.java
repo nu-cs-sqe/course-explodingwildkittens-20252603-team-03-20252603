208de79d60597f3de7e4a0561574e0551f245307
carolineguerraa <carolinejjguerra@gmail.com>
2026-05-31 13:29:52 -0500

    testing & bva: startGame_FourPlayers_GameStateIsActive passes

--- src/test/java/ui/StartGameIntegrationTest.java
+++ src/test/java/ui/StartGameIntegrationTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class StartGameIntegrationTest {
 
 	private static final int THREE_PLAYERS = 3;
+	private static final int FOUR_PLAYERS = 4;
 
 	@Test
 	void startGame_TwoPlayers_GameStateIsActive() {
@@ -29,6 +30,20 @@ public class StartGameIntegrationTest {
 		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
 		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
 		EasyMock.expect(input.promptNumPlayers()).andReturn(THREE_PLAYERS);
+		EasyMock.replay(display, input);
+
+		GameController gc = new GameController(display, input);
+		gc.startGame();
+
+		assertTrue(gc.isGameActive());
+		EasyMock.verify(display, input);
+	}
+
+	@Test
+	void startGame_FourPlayers_GameStateIsActive() {
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		EasyMock.expect(input.promptNumPlayers()).andReturn(FOUR_PLAYERS);
 		EasyMock.replay(display, input);
 
 		GameController gc = new GameController(display, input);
