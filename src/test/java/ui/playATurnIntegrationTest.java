112d3641feb5cf7e7e0c1a2cba44679e37ce9695
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-06-02 00:09:12 -0500

    playATurn_PlayOneCardNotNopedValidCard_CardIsPlayed

--- src/test/java/ui/playATurnIntegrationTest.java
+++ src/test/java/ui/playATurnIntegrationTest.java
@@ -129,4 +129,37 @@ public class playATurnIntegrationTest {
 		assertEquals(before+1, after);
 		assertNotEquals(firstPlayer,  secondPlayer);
 	}
+
+	@Test
+	void playATurn_PlayOneCardNotNopedValidCard_CardIsPlayed(){
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		List<Card> playerHandCards = new ArrayList<>();
+		List<Card> drawPileCards = new ArrayList<>();
+		Card skipCard = new Card(CardType.SKIP, CardName.SKIP, new SkipAction());
+		playerHandCards.add(skipCard);
+		drawPileCards.add(skipCard);
+
+		EasyMock.expect(input.promptNumPlayers()).andReturn(TWO_PLAYERS);
+		display.showMessage(ViewMessages.format("num.players"));
+		EasyMock.expectLastCall().once();
+		display.showCurrentPlayer(EasyMock.isA(Player.class));
+		EasyMock.expect(input.promptPlayerChoice()).andReturn(PlayerChoice.PLAY_CARD).andReturn(PlayerChoice.DONE_PLAYING_CARDS);
+		EasyMock.expect(input.promptCardSelection(EasyMock.isA(Player.class))).andReturn(playerHandCards).once();
+		EasyMock.expect(input.promptNope(EasyMock.isA(Player.class))).andReturn(false);
+
+		EasyMock.replay(display, input);
+
+		GameController gc = new GameController(display, input, realComboValidator(input));
+		Deck deck = new Deck(drawPileCards);
+		gc.startGame(deck, playerHandCards);
+		Player firstPlayer = gc.gameState().getCurrentPlayer();
+		int before = firstPlayer.getHand().size();
+		gc.playATurn();
+		Player secondPlayer = gc.gameState().getCurrentPlayer();
+		int after = firstPlayer.getHand().size();
+
+		assertEquals(before-1, after);
+		assertNotEquals(firstPlayer,  secondPlayer);
+	}
 }
