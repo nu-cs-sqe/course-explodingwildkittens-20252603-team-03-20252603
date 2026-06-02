ec42b696405234203d406d6fb7084c3885c3ac7f
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-06-01 23:56:54 -0500

    playATurn Integration tests: playATurn_PlayZeroCards_GameIsAdvanced is passing

--- src/test/java/ui/playATurnIntegrationTest.java
+++ src/test/java/ui/playATurnIntegrationTest.java
@@ -23,12 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class playATurnIntegrationTest {
-	private static final int ONE_PLAYER = 1;
 	private static final int TWO_PLAYERS = 2;
-	private static final int THREE_PLAYERS = 3;
-	private static final int FOUR_PLAYERS = 4;
-	private static final int FIVE_PLAYERS = 5;
-	private static final int SIX_PLAYERS = 6;
 
 	private static ComboValidator realComboValidator(IPlayerInput input) {
 		return new ComboValidator(new PlayerInteractionHelper(input, new Random()));
@@ -100,8 +95,38 @@ public class playATurnIntegrationTest {
 		Player secondPlayer = gc.gameState().getCurrentPlayer();
 		int after = firstPlayer.getHand().size();
 
-		assertNotEquals(before, after);
+		assertEquals(before+1, after);
 		assertNotEquals(firstPlayer,  secondPlayer);
 
+	}
+
+	@Test
+	void playATurn_PlayZeroCards_GameIsAdvanced(){
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		EasyMock.expect(input.promptNumPlayers()).andReturn(TWO_PLAYERS);
+		display.showMessage(ViewMessages.format("num.players"));
+		EasyMock.expectLastCall().once();
+		display.showCurrentPlayer(EasyMock.isA(Player.class));
+		EasyMock.expect(input.promptPlayerChoice()).andReturn(PlayerChoice.DONE_PLAYING_CARDS);
+		EasyMock.replay(display, input);
+
+		List<Card> cards = new ArrayList<>();
+		Card skipCard = new Card(CardType.SKIP, CardName.SKIP, new SkipAction());
+		Card shuffleCard = new Card(CardType.SHUFFLE, CardName.SKIP, new ShuffleAction());
+		cards.add(skipCard);
+		cards.add(shuffleCard);
+
+		GameController gc = new GameController(display, input, realComboValidator(input));
+		Deck deck = new Deck(cards);
+		gc.startGame(deck, cards);
+		Player firstPlayer = gc.gameState().getCurrentPlayer();
+		int before = firstPlayer.getHand().size();
+		gc.playATurn();
+		Player secondPlayer = gc.gameState().getCurrentPlayer();
+		int after = firstPlayer.getHand().size();
+
+		assertEquals(before+1, after);
+		assertNotEquals(firstPlayer,  secondPlayer);
 	}
 }
