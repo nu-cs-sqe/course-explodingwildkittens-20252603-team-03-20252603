988a212e955bd805caa278be8180d74977cc89ec
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-06-02 00:19:27 -0500

    playATurn_PlayManyCardsNotNopedValidCards_CardsArePlayed is passing

--- src/test/java/ui/playATurnIntegrationTest.java
+++ src/test/java/ui/playATurnIntegrationTest.java
@@ -162,4 +162,42 @@ public class playATurnIntegrationTest {
 		assertEquals(before-1, after);
 		assertNotEquals(firstPlayer,  secondPlayer);
 	}
+
+	@Test
+	void playATurn_PlayManyCardsNotNopedValidCards_CardsArePlayed(){
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		List<Card> playerHandCards = new ArrayList<>();
+		List<Card> drawPileCards = new ArrayList<>();
+		Card skipCard = new Card(CardType.SKIP, CardName.SKIP, new SkipAction());
+		Card shuffleCard = new Card(CardType.SHUFFLE, CardName.SKIP, new ShuffleAction());
+		playerHandCards.add(skipCard);
+		playerHandCards.add(shuffleCard);
+		drawPileCards.add(skipCard);
+
+		EasyMock.expect(input.promptNumPlayers()).andReturn(TWO_PLAYERS);
+		display.showMessage(ViewMessages.format("num.players"));
+		EasyMock.expectLastCall().once();
+		display.showCurrentPlayer(EasyMock.isA(Player.class));
+		EasyMock.expect(input.promptPlayerChoice()).andReturn(PlayerChoice.PLAY_CARD).andReturn(PlayerChoice.PLAY_CARD).andReturn(PlayerChoice.DONE_PLAYING_CARDS);
+		EasyMock.expect(input.promptCardSelection(EasyMock.isA(Player.class)))
+				.andReturn(List.of(playerHandCards.get(0)))
+				.andReturn(List.of(playerHandCards.get(1)));
+		EasyMock.expect(input.promptNope(EasyMock.isA(Player.class))).andReturn(false).andReturn(false);
+
+		EasyMock.replay(display, input);
+
+		GameController gc = new GameController(display, input, realComboValidator(input));
+		Deck deck = new Deck(drawPileCards);
+		gc.startGame(deck, playerHandCards);
+		Player firstPlayer = gc.gameState().getCurrentPlayer();
+		int before = firstPlayer.getHand().size();
+		gc.playATurn();
+		Player secondPlayer = gc.gameState().getCurrentPlayer();
+		int after = firstPlayer.getHand().size();
+
+		assertEquals(before-2, after);
+		assertNotEquals(firstPlayer,  secondPlayer);
+
+	}
 }
