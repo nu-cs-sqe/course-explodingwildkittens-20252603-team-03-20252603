df1ce967438c44468eaa061d68ca121051ec22bf
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-06-02 03:24:57 -0500

    added skip_IsNopedOnce_DoesNotSkip tests and bva

--- src/test/java/ui/SkipIntegrationTest.java
+++ src/test/java/ui/SkipIntegrationTest.java
@@ -72,4 +72,45 @@ public class SkipIntegrationTest {
 	}
 
 
+	@Test
+	void skip_IsNopedOnce_DoesNotSkip(){
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		List<Card> playerHandCards = new ArrayList<>();
+		List<Card> drawPileCards = new ArrayList<>();
+		Card shuffleCard = new Card(CardType.SHUFFLE, CardName.SHUFFLE, new ShuffleAction());
+		Card skipCard = new Card(CardType.SKIP, CardName.SKIP, new SkipAction());
+		Card cattermelonCard = new Card(CardType.CAT_CARD, CardName.CATTERMELON, new NoAction());
+		drawPileCards.add(skipCard);
+		drawPileCards.add(cattermelonCard);
+		drawPileCards.add(shuffleCard);
+		playerHandCards.add(skipCard);
+
+		EasyMock.expect(input.promptNumPlayers()).andReturn(TWO_PLAYERS);
+		display.showMessage(ViewMessages.format("num.players"));
+		EasyMock.expectLastCall().once();
+		display.showCurrentPlayer(EasyMock.isA(Player.class));
+		EasyMock.expect(input.promptPlayerChoice())
+				.andReturn(PlayerChoice.PLAY_CARD)
+				.andReturn(PlayerChoice.DONE_PLAYING_CARDS);
+		EasyMock.expect(input.promptCardSelection(EasyMock.isA(Player.class)))
+				.andReturn(List.of(skipCard)).once();
+		EasyMock.expect(input.promptNope(EasyMock.isA(Player.class))).andReturn(true);
+
+		EasyMock.replay(display, input);
+
+		GameController gc = new GameController(display, input, realComboValidator(input));
+		Deck deck = new Deck(drawPileCards);
+		gc.startGame(deck, playerHandCards);
+		Player firstPlayer = gc.gameState().getCurrentPlayer();
+		int sizeBefore = firstPlayer.getHand().size();
+		gc.playATurn();
+		Player secondPlayer = gc.gameState().getCurrentPlayer();
+		int sizeAfter = firstPlayer.getHand().size();
+
+		assertEquals(sizeBefore, sizeAfter);
+		assertNotEquals(firstPlayer,  secondPlayer);
+	}
+
+
 }
