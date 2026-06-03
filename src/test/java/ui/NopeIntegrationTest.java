d2dcbe142201b2f6393b6a79a931ce2ac1c1f0c1
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-06-03 00:13:55 -0500

    nope_ZeroNopes_CardExecuted test passing: tests and bva

--- src/test/java/ui/NopeIntegrationTest.java
+++ src/test/java/ui/NopeIntegrationTest.java
@@ -32,6 +32,52 @@ public class NopeIntegrationTest {
 	}
 
 	@Test
+	void nope_ZeroNopes_CardExecuted(){
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		List<Card> playerHandCards = new ArrayList<>();
+		List<Card> drawPileCards = new ArrayList<>();
+		Card shuffleCard = new Card(CardType.SHUFFLE, CardName.SHUFFLE, new ShuffleAction());
+		Card skipCard = new Card(CardType.SKIP, CardName.SKIP, new SkipAction());
+		Card cattermelonCard = new Card(CardType.CAT_CARD, CardName.CATTERMELON, new NoAction());
+		Card nopeCard1 = new Card(CardType.NOPE, CardName.NOPE, new NoAction());
+		Card nopeCard2 = new Card(CardType.NOPE, CardName.NOPE, new NoAction());
+		Card nopeCard3 = new Card(CardType.NOPE, CardName.NOPE, new NoAction());
+		playerHandCards.add(nopeCard1);
+		playerHandCards.add(nopeCard2);
+		playerHandCards.add(nopeCard3);
+		drawPileCards.add(skipCard);
+		drawPileCards.add(cattermelonCard);
+		drawPileCards.add(shuffleCard);
+		playerHandCards.add(skipCard);
+
+		EasyMock.expect(input.promptNumPlayers()).andReturn(TWO_PLAYERS);
+		display.showCurrentPlayer(EasyMock.isA(Player.class));
+		EasyMock.expect(input.promptPlayerChoice())
+				.andReturn(PlayerChoice.PLAY_CARD)
+				.andReturn(PlayerChoice.DONE_PLAYING_CARDS);
+		EasyMock.expect(input.promptCardSelection(EasyMock.isA(Player.class)))
+				.andReturn(List.of(skipCard)).once();
+		EasyMock.expect(input.promptNope(EasyMock.isA(Player.class))).andReturn(false);
+
+		EasyMock.replay(display, input);
+
+		GameController gc = new GameController(display, input, realComboValidator(input));
+		Deck deck = new Deck(drawPileCards);
+		gc.startGame(deck, playerHandCards);
+		Player firstPlayer = gc.gameState().getCurrentPlayer();
+		int sizeBefore = firstPlayer.getHand().size();
+		gc.playATurn();
+		Player secondPlayer = gc.gameState().getCurrentPlayer();
+		int sizeAfter = firstPlayer.getHand().size();
+
+		assertEquals(sizeBefore-1, sizeAfter);
+		assertNotEquals(firstPlayer,  secondPlayer);
+
+		EasyMock.verify(display, input);
+	}
+
+	@Test
 	void nope_OneNope_CardNotExecuted(){
 		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
 		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
