d2e1726bf81ce1f6d9b81eaf0f51cf25d718ee69
Mercy Muiruri <mercymuiruri2027@u.northwestern.edu>
2026-06-03 00:20:22 -0500

    nope_ThreeNopes_CardNotExecuted test passing: tests and bva

--- src/test/java/ui/NopeIntegrationTest.java
+++ src/test/java/ui/NopeIntegrationTest.java
@@ -27,6 +27,7 @@ public class NopeIntegrationTest {
 	private static final int TWO_PLAYERS = 2;
 	private static final int ONE_CARD = 1;
 	private static final int THREE_PLAYERS = 3;
+	private static final int FOUR_PLAYERS = 4;
 
 	private static ComboValidator realComboValidator(IPlayerInput input) {
 		return new ComboValidator(new PlayerInteractionHelper(input, new Random()));
@@ -173,6 +174,58 @@ public class NopeIntegrationTest {
 
 		EasyMock.verify(display, input);
 
+	}
+
+
+	@Test
+	void nope_ThreeNopes_CardNotExecuted(){
+		IGameDisplay display = EasyMock.createMock(IGameDisplay.class);
+		IPlayerInput input = EasyMock.createMock(IPlayerInput.class);
+		List<Card> playerHandCards = new ArrayList<>();
+		List<Card> drawPileCards = new ArrayList<>();
+		Card shuffleCard = new Card(CardType.SHUFFLE, CardName.SHUFFLE, new ShuffleAction());
+		Card skipCard = new Card(CardType.SKIP, CardName.SKIP, new SkipAction());
+		Card cattermelonCard = new Card(CardType.CAT_CARD, CardName.CATTERMELON, new NoAction());
+		Card nopeCard1 = new Card(CardType.NOPE, CardName.NOPE, new NoAction());
+		Card nopeCard2 = new Card(CardType.NOPE, CardName.NOPE, new NoAction());
+		Card nopeCard3 = new Card(CardType.NOPE, CardName.NOPE, new NoAction());
+		playerHandCards.add(nopeCard1);
+		playerHandCards.add(nopeCard2);
+		playerHandCards.add(nopeCard3);
+		drawPileCards.add(skipCard);
+		drawPileCards.add(cattermelonCard);
+		drawPileCards.add(shuffleCard);
+		playerHandCards.add(skipCard);
+
+		EasyMock.expect(input.promptNumPlayers()).andReturn(FOUR_PLAYERS);
+		display.showCurrentPlayer(EasyMock.isA(Player.class));
+		EasyMock.expect(input.promptPlayerChoice())
+				.andReturn(PlayerChoice.PLAY_CARD)
+				.andReturn(PlayerChoice.DONE_PLAYING_CARDS);
+		EasyMock.expect(input.promptCardSelection(EasyMock.isA(Player.class)))
+				.andReturn(List.of(skipCard)).once();
+		EasyMock.expect(input.promptNope(EasyMock.isA(Player.class)))
+				.andReturn(true)
+				.andReturn(true)
+				.andReturn(true);
+
+		EasyMock.replay(display, input);
+
+		GameController gc = new GameController(display, input, realComboValidator(input));
+		Deck deck = new Deck(drawPileCards);
+		gc.startGame(deck, playerHandCards);
+		Player firstPlayer = gc.gameState().getCurrentPlayer();
+		int sizeBefore = firstPlayer.getHand().size();
+		gc.playATurn();
+		Player secondPlayer = gc.gameState().getCurrentPlayer();
+		int sizeAfter = firstPlayer.getHand().size();
+		int sizeOfSecondPlayerHand = secondPlayer.getHand().size();
+
+		assertEquals(sizeBefore, sizeAfter);
+		assertEquals(sizeBefore-ONE_CARD, sizeOfSecondPlayerHand);
+		assertNotEquals(firstPlayer,  secondPlayer);
+
+		EasyMock.verify(display, input);
 	}
 
 
